3bd28e4e59cc4c4d1f7c34af576454dabee33a45
Emmanuel <Emmanuel@MBPm1.local>
2026-07-28 13:19:54 +0200

    first commit

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(99):
-    print("{:02d}".format(i), end=", ")
-print("{:02d}".format(99))
+for i in range(9):
+    for j in range(i + 1, 10):
+#        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
