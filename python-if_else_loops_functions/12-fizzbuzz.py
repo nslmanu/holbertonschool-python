408ebc90d22db3706ce49280b30c0eeb90b3f9e5
nslmanu <nslmanu@me.com>
2026-07-28 15:55:54 +0200

    first commit

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-
-#modulo 3 show Fizz and not number 
-#modulo 5 show Buzz
-#modulo 3 et 5 show FizzBuzz
-#last = abs(number) % 10
-#for x in range(97, 123):
-#    if x != 113 and x != 101:
-
 def fizzbuzz():
 	for i in range(1, 101):
 		if i % 15 == 0:
@@ -17,10 +9,3 @@ def fizzbuzz():
 			print("Fizz", end=" ")
 		else:
 			print(i, end=" ")
-
-
-
-#for i in range(1, 101):
-#    print("{:02d}".format(i), end=", ")
-#print("{:02d}".format(99))
-
